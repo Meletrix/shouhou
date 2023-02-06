b81470af59f80b498c929fdc4b95330b0478e364
GG <zjjhgzh@gmail.com>
2023-02-06 19:38:45 +0800

    color update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import { reactive } from "vue";
+
+const themeOverrides = reactive({
+  common: {
+    primaryColor: "#C3A370",
+    primaryColorHover: "#C3A370",
+  },
+});
 </script>
 
 <template>
-  <n-message-provider>
-    <RouterView />
-  </n-message-provider>
+  <n-config-provider :theme-overrides="themeOverrides">
+    <n-message-provider>
+      <RouterView />
+    </n-message-provider>
+  </n-config-provider>
 </template>
 
 <style scoped></style>
