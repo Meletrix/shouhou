37d21f70a214408e783a194964ff9e7944664f07
GG <zjjhgzh@gmail.com>
2023-02-08 01:49:13 +0800

    update

--- src/components/PhotoView.vue
+++ src/components/PhotoView.vue
@@ -1,33 +1,27 @@
 <template>
-  <!--  :disabled="
-      aftersale.phone_number?.length != 11 ||
-      aftersale.order_number == null ||
-      aftersale.order_type == null
+  <!--  
     " -->
   <n-upload
     ref="uploadRef"
-    :file-list="fileList"
     :headers="{
       'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/x-www-form-urlencoded',
     }"
-    action="http://127.0.0.1:9000/api/shouhou/meletrix"
+    action="http://127.0.0.1:9001/api/aftersales/meletrix"
+    :data="{ phone: aftersale.phone_number, id: temp_id }"
     list-type="image-card"
-    :default-upload="false"
-    multiple
     @change="handleChange"
+    @remove="onRemove"
+    @before-upload="beforeUpload"
     :max="3"
     accept="image/png, image/jpeg"
+    :disabled="
+      aftersale.phone_number?.length != 11 ||
+      aftersale.order_number == null ||
+      aftersale.order_type == null
+    "
   >
     +
   </n-upload>
-  <n-button
-    :disabled="!fileListLength"
-    style="margin-bottom: 12px"
-    @click="handleClick"
-  >
-    上传文件
-  </n-button>
 </template>
 
 <script setup lang="ts">
@@ -35,19 +29,39 @@ import { ref } from "vue";
 import { useMessage } from "naive-ui";
 import type { UploadFileInfo, UploadInst } from "naive-ui";
 import { useAfterSale } from "@/stores/shouhou";
+import axios from "axios";
 const message = useMessage();
 const aftersale = useAfterSale();
 
 const fileListLength = ref(0);
 const uploadRef = ref<UploadInst | null>(null);
-const fileList = ref<UploadFileInfo[]>([]);
 const handleChange = (options: { fileList: UploadFileInfo[] }) => {
   fileListLength.value = options.fileList.length;
-  fileList.value = options.fileList;
 };
-const handleClick = () => {
-  uploadRef.value?.submit();
-  message.info("上传文件");
+const temp_id = ref("");
+
+const beforeUpload = (options: {
+  file: UploadFileInfo;
+  fileList: UploadFileInfo[];
+}) => {
+  temp_id.value = options.file.id;
+};
+
+const onRemove = async (options: {
+  file: UploadFileInfo;
+  fileList: Array<UploadFileInfo>;
+}) => {
+  const temp_data = { phone: aftersale.phone_number, id: options.file.id };
+  try {
+    await axios.post("http://127.0.0.1:9001/api/aftersales/delect", temp_data, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (e: any) {
+    message.error("删除失败: " + e.response!.data.message);
+  }
 };
 </script>
 
