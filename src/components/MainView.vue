eb651115de296b5812f9cc5ab8247fccc0c08c5a
GG <zjjhgzh@gmail.com>
2023-02-08 02:55:04 +0800

    update

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -31,7 +31,29 @@
           path="order_photo"
           label="请添加与您售后相关的图片(请先填写其他信息)"
         >
-          <Photo />
+          <n-upload
+            ref="uploadRef"
+            :headers="{
+              'Access-Control-Allow-Origin': '*',
+            }"
+            action="https://120.79.0.147:6666/api/aftersales/meletrix"
+            :data="{ phone: aftersale.phone_number, id: temp_id }"
+            :default-upload="false"
+            list-type="image-card"
+            @change="handleChange"
+            @remove="onRemove"
+            @before-upload="beforeUpload"
+            @finish="finish"
+            :max="3"
+            accept="image/png, image/jpeg"
+            :disabled="
+              aftersale.phone_number?.length != 11 ||
+              aftersale.order_number == null ||
+              aftersale.order_type == null
+            "
+          >
+            +
+          </n-upload>
         </n-form-item>
         <div style="text-align: center">
           <n-button
@@ -47,7 +69,7 @@
             type="primary"
             size="large"
             attr-type="submit"
-            @click="signinClick"
+            @click="doClick"
             style="margin: 4px 2px; padding: 20px 50px"
           >
             提交
@@ -64,14 +86,16 @@ import type { FormInst, FormItemRule, FormRules } from "naive-ui";
 import { useMessage } from "naive-ui";
 import { ref } from "vue";
 import Cascader from "./CascaderView.vue";
-import Photo from "./PhotoView.vue";
+import type { UploadFileInfo, UploadInst } from "naive-ui";
+import axios from "axios";
 
 const formRef = ref<FormInst | null>(null);
 const message = useMessage();
 const aftersale = useAfterSale();
 const hoverable = ref(true);
 
-const signinClick = () => {
+const doClick = () => {
+  uploadRef.value?.submit;
   console.log(aftersale.$state);
   message.success("提交成功");
 };
@@ -116,6 +140,43 @@ const rules: FormRules = {
       trigger: ["input", "blur"],
     },
   ],
+};
+
+const fileListLength = ref(0);
+const uploadRef = ref<UploadInst | null>(null);
+const handleChange = (options: { fileList: UploadFileInfo[] }) => {
+  fileListLength.value = options.fileList.length;
+};
+const temp_id = ref("");
+
+const beforeUpload = (options: {
+  file: UploadFileInfo;
+  fileList: UploadFileInfo[];
+}) => {
+  temp_id.value = options.file.id;
+};
+const finish = (options: { file: UploadFileInfo; event?: ProgressEvent }) => {
+  message.success("文件: " + options.file.name + "上传成功");
+};
+const onRemove = async (options: {
+  file: UploadFileInfo;
+  fileList: Array<UploadFileInfo>;
+}) => {
+  const temp_data = { phone: aftersale.phone_number, id: options.file.id };
+  try {
+    await axios.post(
+      "https://120.79.0.147:6666/api/aftersales/meletrix/delect",
+      temp_data,
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      }
+    );
+    message.success("文件: " + options.file.name + "删除成功");
+  } catch (e: any) {
+    message.error("删除失败: " + e.response!.data.message);
+  }
 };
 </script>
 
